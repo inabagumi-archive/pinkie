c6d4ec428b93f0b2bb33292b3bb74bb68c5272fb
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-05-29 01:22:44 +0900

    refactor(thumbnail): extract func (#99)

--- pkg/thumbnail/thumbnail.go
+++ pkg/thumbnail/thumbnail.go
@@ -11,6 +11,47 @@ import (
 	"golang.org/x/image/draw"
 )
 
+func Fetch(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		resp.Body.Close()
+
+		return nil, fmt.Errorf("thumbnail: invalid status %d", resp.StatusCode)
+	}
+
+	return resp, nil
+}
+
+func Scale(img image.Image) image.Image {
+	rect := img.Bounds()
+	width := rect.Dx()
+	height := rect.Dy()
+
+	newRect := image.Rect(0, 0, 10, 10*height/width)
+	newImg := image.NewRGBA(newRect)
+
+	draw.BiLinear.Scale(newImg, newImg.Bounds(), img, rect, draw.Over, nil)
+
+	return newImg
+}
+
+func EncodeToDataURL(img image.Image) (string, error) {
+	buf := bytes.NewBuffer(nil)
+
+	err := jpeg.Encode(buf, img, &jpeg.Options{Quality: 70})
+	if err != nil {
+		return "", err
+	}
+
+	data := base64.StdEncoding.EncodeToString(buf.Bytes())
+
+	return "data:image/jpeg;base64," + data, nil
+}
+
 type Thumbnail struct {
 	Height int    `json:"height"`
 	PreSrc string `json:"preSrc"`
@@ -19,44 +60,31 @@ type Thumbnail struct {
 }
 
 func New(id string, size string) (*Thumbnail, error) {
-	src := "https://i.ytimg.com/vi/" + id + "/" + size + "default.jpg"
-	res, err := http.Get(src)
+	src := fmt.Sprintf("https://i.ytimg.com/vi/%s/%sdefault.jpg", id, size)
+
+	resp, err := Fetch(src)
 	if err != nil {
 		return nil, err
 	}
-	defer res.Body.Close()
+	defer resp.Body.Close()
 
-	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("thumbnail: invalid status %d", res.StatusCode)
-	}
-
-	img, _, err := image.Decode(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	bounds := img.Bounds()
-	width := bounds.Dx()
-	height := bounds.Dy()
-
-	newRect := image.Rect(0, 0, 10, 10*height/width)
-	newImg := image.NewRGBA(newRect)
-	draw.BiLinear.Scale(newImg, newImg.Bounds(), img, bounds, draw.Over, nil)
-
-	buf := bytes.NewBuffer(nil)
-	err = jpeg.Encode(buf, newImg, &jpeg.Options{Quality: 70})
+	img, _, err := image.Decode(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	data := base64.StdEncoding.EncodeToString(buf.Bytes())
-	preSrc := "data:image/jpeg;base64," + data
+	lqipImg := Scale(img)
+
+	preSrc, err := EncodeToDataURL(lqipImg)
+	if err != nil {
+		return nil, err
+	}
 
 	t := &Thumbnail{
-		Height: height,
+		Height: img.Bounds().Dy(),
 		PreSrc: preSrc,
 		Src:    src,
-		Width:  width,
+		Width:  img.Bounds().Dx(),
 	}
 
 	return t, nil
